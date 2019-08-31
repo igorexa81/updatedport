c6a0c1bdd114e1d1e04335eca1db1d21426dc59e
Igor Baksht <igorexa81@gmail.com>
2019-08-30 20:12:56 -0600

    new changes

---
+++ assets/port.js
@@ -0,0 +1,10 @@
+function clickBody(){
+    document.getElementById("about-me").style.visibility = "visible";
+    
+        document.getElementById("about_btn").style.visibility = "hidden";
+    
+}
+
+function onInitialization(){
+    document.getElementById("about-me").style.visibility = "hidden";
+}
